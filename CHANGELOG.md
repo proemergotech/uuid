deb89d1bd0d66e847190fc3695b758d60d290a69
Norbert Csibra <norbert.csibra@proemergotech.com>
2022-03-08 11:49:14 +0100

    Sync with gitlab

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,8 @@
 # Release Notes
 
-## v1.0.1 / 2020-11-24
-- migrated to GitHub
+## v1.1.0 / 2022-03-08
+- sync with gitlab
+- update go version
 
 ## v1.0.0 / 2020-01-07
 - release version v1.0.0
--- verify.sh
+++ verify.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-# Check for CHANGELOG updates and fails when not found
+
 changelog_filename="CHANGELOG.md"
 
 git fetch --depth 1 origin master
+
+if ! git diff --name-only origin/master | grep -E '.*\.go|go.mod|go.sum'
+then
+  echo "Go code change not found."
+  exit 0
+fi
+
 if [ -f ${changelog_filename} ]
 then
     if [ "$(git diff --name-only origin/master -- ${changelog_filename} | grep -cw ${changelog_filename})" -ne 1 ]
